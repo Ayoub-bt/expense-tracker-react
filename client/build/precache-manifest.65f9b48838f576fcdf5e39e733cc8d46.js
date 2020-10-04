e9c099c681196366fa1e2e4a1cb5ccdaf16e31c4
Ayoub-bt <boudarbat.ayoub@gmail.com>
2020-10-04 03:57:33 +0100

    initial commit

---
+++ client/build/precache-manifest.65f9b48838f576fcdf5e39e733cc8d46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5fd487a9b3d1763f405f58d61112938",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7191881385e86c63fd3",
+    "url": "/static/css/main.809ab9f6.chunk.css"
+  },
+  {
+    "revision": "6db62c1a07d8f93bdccd",
+    "url": "/static/js/2.173b4836.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.173b4836.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7191881385e86c63fd3",
+    "url": "/static/js/main.0c712e21.chunk.js"
+  },
+  {
+    "revision": "fd1e764c12408bbbbfc1",
+    "url": "/static/js/runtime-main.f859b388.js"
+  }
+]);
